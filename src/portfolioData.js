dd54d02ec9e27197b4b4ffa999f3616f553debd8
Mohamed Ismail <ismailcs2003@gmail.com>
2025-09-07 20:16:04 +0300

    initial

--- src/portfolioData.js
+++ src/portfolioData.js
@@ -3,14 +3,14 @@ export const projects = [
     title: "CareerArc – AI Career Platform",
     description: "Built with React vite, Node.js, Express, MongoDB & integrated with Python ML to analyze resumes and recommend job roles.",
     tags: ['Node.js', 'Python ML', 'React.vite'],
-    github: "https://github.com/ismailcodes-dev/careerarc",
+    github: "https://github.com/ismailcodes-dev/careerarc-2.0",
     bg: "#90caf9"
   },
   {
-    title: "INR Harmonization – Dataset Tool",
-    description: "Python tool to process, clean, and standardize INR image datasets for AI training.",
-    tags: ['Python', 'Image Processing'],
-    github: "https://github.com/ismailcodes-dev/inr-harmonization",
+    title: "Rocks Bot For Discord",
+    description: "Your bot is a multi-functional Discord bot designed to create a comprehensive economy and shop system for a server. It uses two SQLite databases.",
+    tags: ['Python'],
+    github: "https://github.com/ismailcodes-dev/Rocks",
     bg: "#a5d6a7"
   }
 ];
